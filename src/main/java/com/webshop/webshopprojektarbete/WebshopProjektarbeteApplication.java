2dad3a81158099767d0b30ad423a13de0587f52e
Estrid4Ever <johannes-sr@hotmail.com>
2024-03-07 12:39:44 +0100

    project init
    empty project init

---
+++ src/main/java/com/webshop/webshopprojektarbete/WebshopProjektarbeteApplication.java
@@ -0,0 +1,13 @@
+package com.webshop.webshopprojektarbete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopProjektarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopProjektarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webshop/webshopprojektarbete/WebshopProjektarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshop.webshopprojektarbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopProjektarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
